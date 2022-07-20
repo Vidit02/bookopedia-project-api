28dce064a54b0734b95cae6fdd70a20ee87c9a1a
“Vidit <vidit.21102@gmail.com>
2022-07-21 02:13:30 +0530

    OTP Email Template Done

--- src/main/java/com/service/EmailService.java
+++ src/main/java/com/service/EmailService.java
@@ -39,8 +39,26 @@ public class EmailService {
 			MimeMessage mimeMessage = new MimeMessage(session);
 			mimeMessage.setFrom(fromEmail);
 			mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-			mimeMessage.setSubject("Welcome");
-			mimeMessage.setContent("Welcome to bingo.com" + otp, "text/html");
+			mimeMessage.setSubject("Book-o-pedia : OTP to reset your password");
+			String code = "<div style=\"font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2;background-color:#f2f5f6; \">\n"
+					+ "        <div style=\"margin:50px auto;width:70%;padding:20px 0\">\n"
+					+ "          <div style=\"border-bottom:1px solid #eee;text-align: center;\">\n"
+					+ "            <a href=\"\" style=\"font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600;\">\n"
+					+ "              <img src=\"https://i.postimg.cc/bwYFS0tJ/android-chrome-256x256.png\" width=\"100\" >\n"
+					+ "            </a>\n"
+					+ "          </div>\n"
+					+ "          <p style=\"font-size:1.1em\">Hi,</p>\n"
+					+ "          <p>Thank you for Book-o-pedia. Use the following OTP to reset your password. OTP is valid for 90 seconds</p>\n"
+					+ "          <h2 style=\"background:black;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;\">"+otp+"</h2>\n"
+					+ "          <p style=\"font-size:0.9em;\">Regards,<br />Book-o-pedia</p>\n"
+					+ "          <hr style=\"border:none;border-top:1px solid #eee\" />\n"
+					+ "          <div style=\"float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300\">\n"
+					+ "            <p>Book-o-pedia</p>\n"
+					+ "            <p>Ahmedabad</p>\n"
+					+ "          </div>\n"
+					+ "        </div>\n"
+					+ "      </div>";
+			mimeMessage.setContent(code, "text/html");
 			
 			Transport.send(mimeMessage);
 			System.out.println("email sent");
